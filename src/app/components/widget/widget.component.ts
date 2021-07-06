7044fa1f60bc97d897cbb5ff3115480ee196b0de
Locottus <herlich@gmail.com>
2021-07-06 11:07:33 -0600

    layout forecast

--- src/app/components/widget/widget.component.ts
+++ src/app/components/widget/widget.component.ts
@@ -58,6 +58,9 @@ export class WidgetComponent implements OnInit {
         this.longitude = position.coords.longitude;
         console.log(position.coords.latitude,position.coords.longitude);
         console.log(this.date);
+        //https://api.openweathermap.org/data/2.5/forecast?lat=14.6341888&lon=-90.5248768&appid=98674de6a91859bcea48ba07be964379&units=metric&lang=sp
+        //https://openweathermap.org/forecast5
+        
       });
     } else {
       console.log("Geolocation is not supported by this browser.");
