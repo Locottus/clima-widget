753a2fc56c65e04cc6d59a21b7d1012d16afc510
Locottus <herlich@gmail.com>
2021-07-07 10:05:58 -0600

    adding mini widget layout

--- src/app/components/mini-widget/mini-widget.component.ts
+++ src/app/components/mini-widget/mini-widget.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { 
+  faCloud,faCloudMoon,faCloudMoonRain,faCloudRain,
+  faCloudShowersHeavy,faCloudSun,faCloudSunRain,
+  faMoon,faSun,faThermometer,faWind,faEye,faTint,
+  faArrowDown,faThermometerFull,faThermometerEmpty
+ } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-mini-widget',
@@ -6,6 +12,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mini-widget.component.css']
 })
 export class MiniWidgetComponent implements OnInit {
+  fathermometerfull=faThermometerFull;
+  fathermometerempty=faThermometerEmpty;
+  faarrowdown = faArrowDown;
+  fatint = faTint;
+  facloud = faCloud;
+  facloudmoon = faCloudMoon;
+  facloudmoonrain = faCloudMoonRain;
+  facloudrain = faCloudRain;
+  facloudshowersheavy = faCloudShowersHeavy;
+  facloudsun = faCloudSun;
+  facloudsunrain = faCloudSunRain;
+  famoon = faMoon;
+  fasun = faSun;
+  fathermometer = faThermometer;
+  fawind =faWind;
+  faeye = faEye;
+
+  date = new Date();
+  latitude = 0;
+  longitude = 0;
+
+  nubosidad = 0;
+  visibilidad = 0;
+  humedad = 0;
+  presion = 0;
+  altura = 0;
+  temperatura = 0;
+  max = 0;
+  min = 0;
+  feels = 0;
+  vientoGrados = 0;
+  vientoGust = 0;
+  vientoVelocidad = 0;
+
+  climaDescripcion = 'parcialmente nublado';
 
   constructor() { }
 
